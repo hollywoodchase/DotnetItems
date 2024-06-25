4ba030cd1d88bb21ae547c5a6ce9d27a0ae36493
Danny Calise <32149505+hollywoodchase@users.noreply.github.com>
2024-06-25 18:24:56 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddSession();  // Add session services
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.UseSession();  // Use session
+
+app.MapRazorPages();
+
+app.Run();
